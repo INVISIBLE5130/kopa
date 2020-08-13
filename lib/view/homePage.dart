f215ed866d0ed47b377204a795fed558b189bddd
INVISIBLE5130 <goshakovan14@gmail.com>
2020-08-13 23:28:40 +0300

    Added main page with cards

--- lib/view/homePage.dart
+++ lib/view/homePage.dart
@@ -13,7 +13,107 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Color.fromARGB(1,229,229,229),
+      body: Container(
+        alignment: Alignment.topRight,
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topRight,
+              child: Image(
+                image: AssetImage("assets/header.png"),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 70, left: 30),
+                child: Image(
+                  image: AssetImage("assets/filter.png"),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: new ListView.builder(
+                padding: const EdgeInsets.all(16),
+                itemBuilder: (context, i) {
+                  return Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(25.0),
+                      child: Card(
+                        color: Color.fromARGB(100, 100, 100, 100),
+                        child: InkWell(
+                          splashColor: Colors.yellow.withAlpha(30),
+                          onTap: () {
+                            print('Card tapped.');
+                          },
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                width: 150.0,
+                                height: 130.0,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/item.png'),
+                                      fit: BoxFit.cover),
+                                  borderRadius:
+                                  BorderRadius.only(topRight: Radius.circular(15.0), bottomRight: Radius.circular(15.0)),
+                                  boxShadow: [
+                                    BoxShadow(blurRadius: 2.0, color: Colors.black)
+                                  ]
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  return showDialog<void>(
+                                    context: context,
+                                    barrierDismissible: false,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('Not in stock'),
+                                        content:
+                                        const Text('This item is no longer available'),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                            child: Text('Ok'),
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                    padding: EdgeInsets.only(left: 160, bottom: 80),
+                                    child: Chip(
+                                      label: Text(
+                                        "100" + new String.fromCharCodes(new Runes('\u0024')),
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16
+                                        ),
+                                      ),
+                                      shadowColor: Colors.black,
+                                      backgroundColor: Colors.yellow,
+                                      elevation: 10,
+                                      autofocus: true,
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
