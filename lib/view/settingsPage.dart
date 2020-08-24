8d2b5113a7909ad9a9f6e834a899265f145620ed
INVISIBLE5130 <goshakovan14@gmail.com>
2020-08-24 21:33:51 +0300

    Added fixes to settings screen && opportunity to set profile photo

--- lib/view/settingsPage.dart
+++ lib/view/settingsPage.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart'; // For File Upload To Firestore
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // For Image Picker
+import 'package:path/path.dart' as Path;
 
 class SettingsPage extends StatefulWidget {
 
@@ -12,9 +15,31 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool isLoggedIn = false;
-  bool pressed = false;
   Map userProfile;
+  File _image;
+  String _uploadedFileURL;
+
+  Future chooseFile() async {
+    await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {
+      setState(() {
+        _image = image;
+      });
+    });
+  }
+
+  Future uploadFile() async {
+    StorageReference storageReference = FirebaseStorage.instance
+        .ref()
+        .child('userImages/${Path.basename(_image.path)}}');
+    StorageUploadTask uploadTask = storageReference.putFile(_image);
+    await uploadTask.onComplete;
+    print('Image Uploaded');
+    storageReference.getDownloadURL().then((fileURL) {
+      setState(() {
+        _uploadedFileURL = fileURL;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,206 +55,68 @@ class _SettingsPageState extends State<SettingsPage> {
                 image: AssetImage("assets/header.png"),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: ListView.builder(
-                padding: const EdgeInsets.all(16),
-                itemCount: 3,
-                itemBuilder: (context, i) {
-                  return Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(25.0),
-                      child: Card(
-                        color: Color.fromARGB(100, 100, 100, 100),
-                        child: InkWell(
-                          splashColor: Colors.yellow.withAlpha(30),
-                          onTap: () {
-                            print('Card tapped.');
-                          },
-                          child: Stack(
-                            children: <Widget>[
-                              Container(
-                                width: 150.0,
-                                height: 130.0,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: AssetImage('assets/item.png'),
-                                        fit: BoxFit.cover),
-                                    borderRadius:
-                                    BorderRadius.only(topRight: Radius.circular(15.0), bottomRight: Radius.circular(15.0)),
-                                    boxShadow: [
-                                      BoxShadow(blurRadius: 2.0, color: Colors.black)
-                                    ]
-                                ),
-                                child: FlatButton(
-                                  onPressed: () {
-                                    setState(() => { pressed = !pressed });
-                                    widget.onPressed();
-                                  },
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 15),
-                                    child: Align(
-                                      alignment: Alignment.topLeft,
-                                      child: Icon(
-                                          Icons.favorite,
-                                          color: pressed ? Colors.red : Colors.white
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  return showDialog<void>(
-                                    context: context,
-                                    barrierDismissible: false,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: Text('Not in stock'),
-                                        content:
-                                        const Text('This item is no longer available'),
-                                        actions: <Widget>[
-                                          FlatButton(
-                                            child: Text('Ok'),
-                                            onPressed: () {
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                        ],
-                                      );
-                                    },
-                                  );
-                                },
-                                child: Container(
-                                    padding: EdgeInsets.only(left: 310, bottom: 80),
-                                    child: Chip(
-                                      label: Text(
-                                        "100" + new String.fromCharCodes(new Runes('\u0024')),
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16
-                                        ),
-                                      ),
-                                      shadowColor: Colors.black,
-                                      backgroundColor: Colors.yellow,
-                                      elevation: 10,
-                                      autofocus: true,
-                                    )
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(left: 170, top: 10),
-                                child: Text(
-                                  "Nike 992K",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(left: 170, top: 35),
-                                child: Text(
-                                  "Розміри стопи: ",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(left: 170, top: 60),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: <Widget>[
-                                    Column(
-                                      children: <Widget>[
-                                        Text(
-                                          "40",
-                                          style: TextStyle(
-                                              color: Colors.blue,
-                                              fontSize: 22
-                                          ),
-                                        ),
-                                        Text(
-                                          "EU",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 10
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 15),
-                                      child: Column(
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.only(bottom: 3),
-                                            child: Text(
-                                              "28.5",
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 14
-                                              ),
-                                            ),
-                                          ),
-                                          Text(
-                                            "Довжина / см",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 15),
-                                      child: Column(
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.only(bottom: 3),
-                                            child: Text(
-                                              "10",
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 14
-                                              ),
-                                            ),
-                                          ),
-                                          Text(
-                                            "Ширина / см",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 10
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(left: 170, top: 105),
-                                child: Text(
-                                  "Матеріал: Шкіра",
-                                  style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 10
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 60, left: 30),
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: _uploadedFileURL != null
+                              ? Image.network(_uploadedFileURL)
+                              : AssetImage('assets/defaultUserImage.png'),
+                          height: 100,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.yellow),
+                            borderRadius: BorderRadius.all(Radius.circular(50))
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 60, left: 50),
+                        child: _image == null
+                            ? FloatingActionButton(
+                                onPressed: chooseFile,
+                                child: CircleAvatar(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 35,
+                                    ),
+                                    backgroundColor: Colors.lightBlueAccent,
+                                ),
+                                backgroundColor: Colors.lightBlueAccent,
+                            )
+                            : FloatingActionButton(
+                                onPressed: uploadFile,
+                                child: CircleAvatar(
+                                  child: Icon(
+                                    Icons.check,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                  backgroundColor: Colors.lightBlueAccent,
+                                ),
+                                backgroundColor: Colors.lightBlueAccent,
+                            ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 45, left: 20),
+                  child: Text(
+                    'Guest',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
                     ),
-                  );
-                },
-              ),
-            ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
