f659e3a2d91b9e9d6d3135fd89a2a7c7c3f8758a
INVISIBLE5130 <goshakovan14@gmail.com>
2020-09-13 13:31:54 +0300

    main was moved to lib

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:kopa/core/ui/mainViewStatefulWidget.dart';
 
-import '../locator.dart';
+import 'locator.dart';
 
 void main() {
   setupLocator();
